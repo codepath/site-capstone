50a82c642c186d6d91fb5d7031e1a6dad17dd9d7
yasminsenior02 <yasminsenior02@gmail.com>
2022-07-25 13:30:09 -0700

    update restroute

--- healthhero/backend/routes/restaurant.js
+++ healthhero/backend/routes/restaurant.js
@@ -4,26 +4,34 @@ const { createUserJwt } = require("../utils/tokens");
 const security = require("../security");
 const router = express.Router();
 
-router.get("/", security.requireAuthenticatedUser, async (req, res, next) => {
-  try {
-    let { id } = res.locals.user;
-    const restaurant = await Restaurant.listRests(id);
-    return res.status(201).json({ restaurant: restaurant });
-  } catch (err) {
-    next(err);
+router.get(
+  "/restaurant",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      let { id } = res.locals.user;
+      const restaurant = await Restaurant.listRests(id);
+      return res.status(201).json({ restaurant: restaurant });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
-  try {
-    let { id } = res.locals.user;
+router.post(
+  "/restaurant",
+  security.requireAuthenticatedUser,
+  async (req, res, next) => {
+    try {
+      let { id } = res.locals.user;
 
-    const restaurant = req.body;
-    const restaurants = await Restaurant.PostRests(restaurant, id);
-    return res.status(201).json({ restaurant: restaurants });
-  } catch (err) {
-    next(err);
+      const restaurant = req.body;
+      const restaurants = await Restaurant.PostRests(restaurant, id);
+      return res.status(201).json({ restaurant: restaurants });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 module.exports = router;
