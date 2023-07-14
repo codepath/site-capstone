1d29a2dc2600ac00e291a1790ac01e90cc51fc7a
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-07-14 15:21:49 -0400

    adds default vite react files, tailwindcss integration, and starter components and directories in capstone-ui and capstone-api

---
+++ codepath_capstone_project/src/capstone-ui/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Homepage from './Homepage'
+import '../index.css'
+
+function App() {
+
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
