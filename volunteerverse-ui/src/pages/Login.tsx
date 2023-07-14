ddfd3f4f7b6a40ebfa62646feee6a376a16e8cdb
Kelechi <kelechi@utexas.edu>
2023-07-14 12:26:30 -0700

    started basic login page

--- volunteerverse-ui/src/pages/Login.tsx
+++ volunteerverse-ui/src/pages/Login.tsx
@@ -1,6 +1,15 @@
+import { Button, Container, Text, Title } from "@mantine/core"
+import { Link } from "react-router-dom";
+import GoBackButton from "../components/GoBackButton";
 
 export default function Login() {
+  console.log("first");
   return (
-    <div>Login</div>
+    <Container>
+      <Title>
+        Login
+      </Title>
+      <GoBackButton />
+    </Container>
   )
 }
