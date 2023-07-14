a1718be97ffbb7b05a0085b0a8b608adec85c580
Iretomiwa Idowu <iiretomiwa1@gmail.com>
2023-07-14 12:31:33 -0700

    commit message

---
+++ volunteerverse-api/models/organization.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+const db = require("../db");
+const bcrypt = require ("bcrypt")
+const { BadRequestError, UnauthorizedError } = require("../utils/errors");
+
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
+class Organization {
+    /**
+   * Convert a user from the database into a user object that can be viewed publically.
+   * Don't show user's password
+   *
+   *
+   * @param {Organization} organization - user from database
+   * @returns public organization
+   */
+  static _createPublicOrganization(organization) {
+    return {
+     
+    };
+  }
+
+
+
+}
+
+module.exports = Organization
