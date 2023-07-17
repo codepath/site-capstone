e1962486a2169cf4d7dcb14f13d9001ba744229a
melyssa5 <melyssa_cristino@brown.edu>
2023-07-17 09:58:23 -0700

    more setup

--- volunteerverse-api/src/routes/volunteer.ts
+++ volunteerverse-api/src/routes/volunteer.ts
@@ -5,8 +5,6 @@ import { Volunteer } from "../models/volunteer"
 export const volunteerRoutes = express.Router()
 
 
-
-
 volunteerRoutes.post("/register", async function (req, res, next) {
   try {
     const volunteer = await Volunteer.register(req.body)
