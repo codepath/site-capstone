54bc1beca6ad609f1180b275b21943bdbca19e31
Leena Abdelrahman <labde2@illinois.edu>
2023-07-17 14:44:22 -0500

    pushing all current changes

--- src/capstone-api/models/hotels.js
+++ src/capstone-api/models/hotels.js
@@ -3,8 +3,10 @@ const { rapidapikey, rapidapihost } = require("../constants");
 const axios = require('axios');
 
 class Hotels {
-  static async hotelsList() { // Mark the method as async
-    console.log("entered");
+   //This is all the Hotel Data from Booking.com
+
+  static async HotelsData() { // Mark the method as async
+    console.log("Printing Hotel Data");
 
     const options = {
       method: 'GET',
@@ -25,8 +27,63 @@ class Hotels {
       return response.data;
     } catch (error) {
       console.error(error);
-      throw new BadRequestError("Failed to fetch hotels");
+      throw new BadRequestError("Failed to fetch Hotels Data");
     }
+  }
+
+  //Hotels Detail 
+  static async HotelsDetail() {
+
+    const options = {
+    method: 'GET',
+    url: 'https://booking-com.p.rapidapi.com/v2/hotels/details',
+    params: {
+        hotel_id: '1676161',
+        currency: 'AED',
+        locale: 'en-gb',
+        checkout_date: '2023-09-28',
+        checkin_date: '2023-09-27'
+    },
+    headers: {
+        'X-RapidAPI-Key': rapidapikey,
+        'X-RapidAPI-Host': rapidapihost
+    }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        throw new BadRequestError("Failed to fetch Hotel Detail");
+      }
+  }
+  //Serach Hotels by locations or name 
+  static async searchLocations() {
+
+    const options = {
+    method: 'GET',
+    url: 'https://booking-com.p.rapidapi.com/v1/hotels/locations',
+    params: {
+        name: 'Berlin',
+        locale: 'en-gb'
+    },
+    headers: {
+        'X-RapidAPI-Key': rapidapikey,
+        'X-RapidAPI-Host': rapidapihost
+    }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        throw new BadRequestError("Failed to fetch Search Locations");
+      }
+
   }
 }
 
--- src/capstone-api/routes/hotels.js
+++ src/capstone-api/routes/hotels.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { hotelsList } = require("../models/hotels")
+const { HotelsData, HotelsDetail } = require("../models/hotels")
 
 
-router.get('/hotels', (req, res) => {
-    console.log("entered this endpiont");
-    const products = hotelsList();
+router.get('/hotels-data', (req, res) => {
+    const products = HotelsData();
+    res.send(products);
+});
+  
+
+router.get('/hotels-detail', (req, res) => {
+    const products = HotelsDetail();
     res.send(products);
 });
   
