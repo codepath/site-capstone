f4da96f26ccf8f8d8137cf84a203c87a48768f85
Gregory Caesar <main@Gregorys-MacBook-Pro.local>
2023-07-18 16:06:29 -0400

    implementation of carousel on Homepage.jsx, Navbar.jsx styling, further Homepage.jsx styling

---
+++ capstone-ui/ImageCard.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function ImageCard({ footer=false }) {
+    return (
+        <div className="border border-blue-500 rounded-lg">
+            <img src="./Assets/no_img_tst.png" />
+        </div>
+    )
+}
