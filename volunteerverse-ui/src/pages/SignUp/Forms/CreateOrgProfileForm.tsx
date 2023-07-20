11763bf2de08c53cb88663030324551f9542e80f
Kelechi <kelechi@utexas.edu>
2023-07-20 07:02:36 -0700

    added styling plus implemented a photo upload feature

--- volunteerverse-ui/src/pages/SignUp/Forms/CreateOrgProfileForm.tsx
+++ volunteerverse-ui/src/pages/SignUp/Forms/CreateOrgProfileForm.tsx
@@ -1,50 +1,86 @@
-import React from 'react';
-import { 
-  TextInput, 
-  Image, Textarea ,Checkbox, Button, Group, 
+import React, { useRef } from 'react';
+import {
+  TextInput,
+  Image, Textarea, Checkbox, Button, Group,
   Container, Title, Flex, Box, createStyles,
+  LoadingOverlay,
 } from '@mantine/core';
 import { UseFormReturnType } from '@mantine/form';
 import { OrgFormValues } from '../../../props/forms';
 
 
-const useStyles = createStyles(() => ({
-  container : {
+const useStyles = createStyles((theme) => ({
+  container: {
 
   },
-  image : {
+  image: {
     display: "flex",
 
+  },
+  title: {
+    [theme.fn.smallerThan("md")]: {
+      fontSize: `calc(${theme.fontSizes.xl})`
+    },
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: `calc(${theme.fontSizes.lg} * 1.25)`
+    }
   }
 }))
 
 
-function CreateOrgProfileForm({ form } : {form : UseFormReturnType<OrgFormValues>}) {
+function CreateOrgProfileForm({ form }: { form: UseFormReturnType<OrgFormValues> }) {
   const { classes } = useStyles()
+  const inputRef = useRef<HTMLInputElement>(null);
+  const openFileBrowswer = () => {
+    inputRef.current?.click();
+  }
 
   return (
     <Container mt={"xl"}>
-      <Title mb={"xl"} >Create Your Profile</Title>
+      <Title className={classes.title} mb={"xl"} >Create Your Profile</Title>
       <Flex direction={"column"}>
-        {/* <Flex p={"sm"} align={"center"} justify={"center"}>
-        DROP ZONE TO APPEAR HEAR
-          <Image className={classes.image} radius={"xl"} withPlaceholder width={200} height={200}/>
-        </Flex> */}
+        <Flex direction={"column"} gap={"md"} align={"center"}>
+          <Image
+            width={200}
+            height={200}
+            withPlaceholder
+            mb={"md"}
+            radius={"50%"} />
+          <Button variant="light"
+            radius={"lg"}
+            mb={"xl"}
+            styles={{
+              root: {
+                maxWidth: "30rem"
+              }
+            }}
+            onClick={openFileBrowswer}>Upload Logo</Button>
+          <TextInput
+            onChange={(event) => console.log(event.target.value)}
+            ref={inputRef}
+            radius={"xl"}
+            styles={{ root: { display: "none" } }}
+            type="file" />
+        </Flex>
         <TextInput
+          size={"md"}
+          radius={"xl"}
+          mb={"xl"}
           withAsterisk
           label="Founder(s):"
           description="Please enter the first and last names of the founders seperated by a comma.E.g. 'Jane Doe, Jack Wobs'"
-          placeholder="Founder(s) Name(s)" 
+          placeholder="Founder(s) Name(s)"
           {...form.getInputProps('founders')}
-          />
-        
-          <Textarea withAsterisk
+        />
+        <Textarea
+          radius={"lg"}
+          withAsterisk
           label="Brief Description:"
-          placeholder="Brief Description" 
+          placeholder="Brief Description"
           description="Max: 300 words"
           minRows={5}
           {...form.getInputProps('org_desc')}
-          />
+        />
       </Flex>
     </Container>
   )
