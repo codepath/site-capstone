0232311a7212aa41aa0859180ea3362707020b91
Kelechi <kelechi@utexas.edu>
2023-07-22 06:43:42 -0700

    added post-auth pages

--- volunteerverse-ui/src/App.tsx
+++ volunteerverse-ui/src/App.tsx
@@ -6,6 +6,12 @@ import NotFound from './pages/NotFound';
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
+import useAuthentication from './services/hooks/useAuthentication';
+import MyProjects from './pages/Volunteer/MyProjects';
+import ProjectDetails from './pages/ProjectDetails';
+import CreateProject from './pages/CreateProject';
+import Home from './pages/Home/Home';
+
 
 function App() {
   /**
@@ -19,27 +25,37 @@ function App() {
    * start web scraping data from democracy lab
    * email validation, password security, photo image hosting
    */
-// Appshell is used to navbar overlay across all pages 
+  // Appshell is used to navbar overlay across all pages 
+
   return (
     <>
-      <MantineProvider withGlobalStyles withNormalizeCSS theme={{primaryColor : "violet"}}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={{ primaryColor: "violet" }}>
         <BrowserRouter>
           <AppShell pl={0} pr={0}
-          styles={(theme) => ({
-            main: {padding : "initial 0"},
-            root: { 
-              height: "100%",
-              backgroundImage: theme.fn.gradient({from: theme.primaryColor,  to: theme.white, deg: 180}),
-            }
-          })}
+            styles={(theme) => ({
+              main: { padding: "initial 0" },
+              root: {
+                height: "100%",
+                backgroundImage: theme.fn.gradient({ from: theme.primaryColor, to: theme.white, deg: 180 }),
+              }
+            })}
             header={<Navbar />}>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/signup/organization" element={<SignUp userType="organization" />} />
-            <Route path="/signup/volunteer" element={<SignUp userType="volunteer"/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/signup/organization" element={<SignUp userType="organization" />} />
+              <Route path="/signup/volunteer" element={<SignUp userType="volunteer" />} />
+              <Route path="/login" element={<Login />} />
+              {/* Home displays the Dashboard page and the student projects feed */}
+              <Route path="/home" element={<Home />}> 
+              {/* Displays project by details page for each user role */}
+                <Route path="projects/:projectId" element={<ProjectDetails />} />
+                {/* projects/create is specfic to organizations looking to create a new project */}
+                <Route path="projects/create" element={<CreateProject />} />
+                {/* projects is  specific to the volunteers */}
+                <Route path="projects" element={<MyProjects />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </AppShell>
 
         </BrowserRouter>
