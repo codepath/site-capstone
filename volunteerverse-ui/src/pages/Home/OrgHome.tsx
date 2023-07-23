34368726c1325b0894a41dd5758d89b358e6bac0
Kelechi <kelechi@utexas.edu>
2023-07-23 08:42:30 -0700

    created skeleton

--- volunteerverse-ui/src/pages/Home/OrgHome.tsx
+++ volunteerverse-ui/src/pages/Home/OrgHome.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useAuthenticationUserProp } from '../../services/hooks/useAuthentication'
 
-function OrgHome() {
+function OrgHome({ user } : {user: useAuthenticationUserProp}) {
+  /**
+   * @todo - display dashboard for orgs to view/create/delete projects they've posted
+   */
   return (
     <div>OrgHome</div>
   )
--- volunteerverse-ui/src/pages/Home/VolunteerHome.tsx
+++ volunteerverse-ui/src/pages/Home/VolunteerHome.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useAuthenticationUserProp } from '../../services/hooks/useAuthentication'
 
-function VolunteerHome() {
+function VolunteerHome({ user } : {user: useAuthenticationUserProp}) {
+  /**
+   * @todo: use effect to get all reccomended volunteer options for a user
+   */
   return (
     <div>VolunteerHome</div>
   )
