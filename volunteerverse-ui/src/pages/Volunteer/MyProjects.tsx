db1322b905429e5e79b35804b7bbe6f4debf4f15
Kelechi <kelechi@utexas.edu>
2023-07-24 10:14:36 -0700

    added small todo comments

--- volunteerverse-ui/src/pages/Volunteer/MyProjects.tsx
+++ volunteerverse-ui/src/pages/Volunteer/MyProjects.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-
 function MyProjects() {
+  /**
+   * @todo: 
+   * use 2? tabs to show projects volunteers is approved for, interested in to show projects a student is interested in
+   * use simple card list layout
+   * give volunteers option to remove interest from projects
+   */
   return (
     <div>MyProjects</div>
   )
