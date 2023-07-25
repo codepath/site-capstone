d5d79834262ca5397abdee7ad9a1551e5f287f9c
Kelechi <kelechi@utexas.edu>
2023-07-25 14:01:25 -0700

    refactored to be more re-usable

--- volunteerverse-ui/src/components/Navbar.tsx
+++ volunteerverse-ui/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Button, Header, Group, Text, Container, Image, createStyles, Title, Modal } from "@mantine/core";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../assets/Logo.png";
-import {Anchor,Divider,Center,
-  Box,Burger,Drawer,Collapse,
-  ScrollArea, rem, } from '@mantine/core';
+import {
+  Anchor, Divider, Center,
+  Box, Burger, Drawer, Collapse,
+  ScrollArea, rem,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { useNavigate } from "react-router-dom";
 import SignUpModal from "../pages/Landing/SignUpModal";
@@ -20,9 +22,9 @@ const useStyles = createStyles((theme) => ({
     paddingLeft: theme.spacing.md,
     paddingRight: theme.spacing.md,
     textDecoration: 'none',
-    color: theme.colorScheme === 'dark' ? theme.white : theme.primaryShade,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.colors.violet[6],
     fontWeight: 500,
-    fontSize: theme.fontSizes.sm,
+    fontSize: theme.fontSizes.lg,
 
     [theme.fn.smallerThan('sm')]: {
       height: rem(42),
@@ -54,9 +56,8 @@ const useStyles = createStyles((theme) => ({
     marginTop: theme.spacing.sm,
     padding: `${theme.spacing.md} calc(${theme.spacing.md} * 2)`,
     paddingBottom: theme.spacing.xl,
-    borderTop: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
-    }`,
+    borderTop: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+      }`,
   },
 
   hiddenMobile: {
@@ -77,37 +78,44 @@ interface linkProp {
   path: string,
   id?: string, // use this if needed to select specific links
 }
-export default function Navbar({isAuth, user, removeToken} : 
-  {user : useAuthenticationUserProp, isAuth: boolean, removeToken: () => void}) {
+export default function Navbar({ isAuth, user, removeToken }:
+  { user: useAuthenticationUserProp, isAuth: boolean, removeToken: () => void }) {
   /**
    * @todo use navigation route to change navbar conditionallty
    */
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [showModal, {open : openModal, close: closeModal }] = useDisclosure(false);
+  const [showModal, { open: openModal, close: closeModal }] = useDisclosure(false);
   const navigate = useNavigate();
   const urlLocation = useLocation();
   const { classes, theme } = useStyles();
 
   // Dynamic Nav bar links
   const volunteerLinks = [
-    {label : "Explore", path:"/"},
-    {label : "My Projects", path:"/projects"},
+    { label: "Explore", path: "/" },
+    { label: "My Projects", path: "/projects" },
   ];
   // const orgLinks = [
-    // {label : "Dashboard", path:"/"},
-    // {label : "How It Works", path:"/#"},
-    // {label : "About", path:"/#About"},
+  // {label : "Dashboard", path:"/"},
+  // {label : "How It Works", path:"/#"},
+  // {label : "About", path:"/#About"},
   // ];
   const unAuthLinks = [
-    {label : "Home", path:"/"},
-    {label : "How It Works", path:"/#HowItWorks"},
-    {label : "About", path:"/#About"},
+    { label: "Home", path: "/" },
+    { label: "How It Works", path: "/#HowItWorks" },
+    { label: "About", path: "/#About" },
   ];
 
-  const mapLinksToElements = (links : linkProp[]) => {
+  const mapLinksToElements = (links: linkProp[], withDivider : boolean = false) => {
     return (
-      links.map((link, index) => (
-        <Link key={`${link.path}:${link.label}`} to={link.path} className={classes.link}>{link.label}</Link>
+      links.map((link) => (
+        <>
+        <Link 
+        key={`${link.path}:${link.label}`} 
+        to={link.path}
+        color={theme.colorScheme === 'dark' ? 'violet.7' : 'violet.6'}
+        className={classes.link}>{link.label}</Link>
+        { withDivider && (<Divider key={`${link.path}:+${link.label}`} my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />)}
+        </>
       ))
     )
   }
@@ -124,24 +132,33 @@ export default function Navbar({isAuth, user, removeToken} :
       <></>,
       { isAuth: isAuth, user: user }
     )
-  }, [user, isAuth])
+  }, [user, isAuth]);
+  const fetchCorrectUserLinksMobile = useMemo(() => {
+    // using usememo here is more performant
+    return fetchCorrectUserOption(
+      mapLinksToElements(unAuthLinks, true),
+      mapLinksToElements(volunteerLinks, true),
+      <></>,
+      { isAuth: isAuth, user: user }
+    )
+  }, [user, isAuth]);
   return (
-    <Box pb={120}>
+    <Box >
       <Header height={60} px="md">
         <Group position="apart" sx={{ height: '100%' }}>
-          <Image src={Logo} width={50} height={50} fit="contain" />
+          <Text size={"xl"} fw={600} variant="gradient" gradient={{from: "violet", to : "violet.2", deg:45}}>VolunteerVerse</Text>
           <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
             {fetchCorrectUserLinks}
           </Group>
           <Group className={classes.hiddenMobile}>
             {
-              isAuth ? <Button onClick={removeToken} radius={"lg"} variant="outline">Log Out</Button> :
+              isAuth ? <Button onClick={()=> {removeToken(); navigate("/")}} radius={"lg"} variant="outline">Log Out</Button> :
                 <>
                   <Button radius={"lg"} onClick={() => navigate("/login")} variant="default">Log in</Button>
                   <Button radius={"lg"} onClick={openModal}>Sign up</Button>
                 </>
             }
-            
+
           </Group>
           <Burger opened={drawerOpened} onClick={toggleDrawer} className={classes.hiddenDesktop} />
         </Group>
@@ -152,42 +169,38 @@ export default function Navbar({isAuth, user, removeToken} :
         onClose={closeDrawer}
         size="100%"
         padding="md"
-        title={<Text weight={700} variant="gradient">VolunteerVerse</Text>}
+        title={<Text size={30} weight={700} gradient={{from: "violet", to : "violet.2", deg:45}} variant="gradient">VolunteerVerse</Text>}
         className={classes.hiddenDesktop}
         zIndex={1000000}
       >
         <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
 
-          <Anchor mt={"xl"} href="/" className={classes.link}>
-            Home
-          </Anchor>
-          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
-          <Anchor href="/#HowItWorks" className={classes.link}>
-            How It Works
-          </Anchor>
-          <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
-          <Anchor href="/#About" className={classes.link}>
-            About
-          </Anchor>
+          {fetchCorrectUserLinksMobile}
 
 
           <Group position="center" grow mt={"md"} py="xl" px="md">
-            <Button variant="outline" radius={"xl"} component={Link} to={"/login"}>Login</Button>
-            <Button radius={"xl"} onClick={openModal}>Sign up</Button>
+          {
+              isAuth ? <Button onClick={removeToken} radius={"lg"} variant="outline">Log Out</Button> :
+                <>
+                  <Button radius={"lg"} onClick={() => navigate("/login")} variant="default">Log in</Button>
+                  <Button radius={"lg"} onClick={openModal}>Sign up</Button>
+                </>
+            }
           </Group>
-          
+
         </ScrollArea>
       </Drawer>
-      <Modal 
-        styles={{ inner: {paddingLeft: "2rem"}}}
-        title={<Text  align="center">Select Your Role:</Text>}
+      <Modal
+        styles={{ inner: { paddingLeft: "2rem" } }}
+        title={<Text align="center">Select Your Role:</Text>}
         closeButtonProps={{ 'aria-label': 'Close modal' }}
-        opened={showModal} 
-        onClose={closeModal} 
-        size="auto" 
+        opened={showModal}
+        onClose={closeModal}
+        size="auto"
         centered
         zIndex={1000001}>
-          <SignUpModal close={closeModal} />
-        </Modal>
+        <SignUpModal close={closeModal} />
+      </Modal>
     </Box>
   );
+}
