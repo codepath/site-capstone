2868d026bd7a34941b2e987af03c5115ac2ffdd4
melyssa5 <melyssa_cristino@brown.edu>
2023-07-24 18:13:26 -0700

    refining functions

--- volunteerverse-api/index.js
+++ volunteerverse-api/index.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({headless:"new"});
+  const browser = await puppeteer.launch({headless:"new", defaultViewport:false,});
   const page = await browser.newPage();
-  await page.goto('https://github.com/pittcsc/Summer2024-Internships');
+  await page.goto('https://www.democracylab.org/projects');
   await page.screenshot({path: 'example.png'});
   await browser.close();
 })();
