2450eb0c7347bf01be694efb79083e6a8c664ea6
Kelechi <kelechi@utexas.edu>
2023-07-25 14:02:33 -0700

    added more data constants for testing

--- volunteerverse-ui/src/pages/Volunteer/Home/data.ts
+++ volunteerverse-ui/src/pages/Volunteer/Home/data.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const projectCardData = [
     {
         imageUrl: 'https://st2.depositphotos.com/1796420/7808/v/450/depositphotos_78080722-stock-illustration-web-development-illustration-computer-display.jpg',
         title: 'Sample Project',
@@ -12,3 +12,19 @@ export const data = [
         orgUrl: "https://www.redcross.org/?cid=generic&med=cpc&source=google&scode=RSG00000E017&&&gclid=Cj0KCQjwn_OlBhDhARIsAG2y6zMmRZkpZJWh8o0MbaWQ2b5Yxl9J2QOSr7OoBZ5Uo4UcihyCHm1pwToaAl--EALw_wcB&gclsrc=aw.ds"
       }
 ]
+export const projectDetailsData = {
+    title: "Sample Project",
+    createdAt: Date.now(),
+    description: "This is a sample project description.",
+    orgName: "Sample Organization",
+    orgUrl: "https://www.redcross.org/?cid=generic&med=cpc&source=google&scode=RSG00000E017&&&gclid=Cj0KCQjwn_OlBhDhARIsAG2y6zMmRZkpZJWh8o0MbaWQ2b5Yxl9J2QOSr7OoBZ5Uo4UcihyCHm1pwToaAl--EALw_wcB&gclsrc=aw.ds",
+    imageUrl: 'https://st2.depositphotos.com/1796420/7808/v/450/depositphotos_78080722-stock-illustration-web-development-illustration-computer-display.jpg',
+    orgDesc: "A brief description of the sample organization.",
+    founders: "John Doe, Jane Smith",
+    interested: true,
+    neededVolunteers: 10,
+    approvedVolunteers: 5,
+    approved: false,
+    tags: ["sample", "project", "typescript"],
+    id: 3
+}
