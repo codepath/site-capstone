122cb8b4339197ed39346ba0b466c33afb50ee3f
Kelechi <kelechi@utexas.edu>
2023-07-26 14:10:45 -0700

    added project details page

---
+++ volunteerverse-ui/src/pages/Org/OrgProjectDetails.tsx
@@ -0,0 +1,14 @@
+import { createStyles } from "@mantine/styles";
+
+const useStyles = createStyles((theme) => ({
+
+}));
+
+function OrgProjectDetails() {
+    const { classes } = useStyles()
+    return (
+        <div>OrgProjectDetails</div>
+    )
+}
+
+export default OrgProjectDetails
