feae0b3188701283b2cf8aa8b16f2a749952ff16
Kelechi <kelechi@utexas.edu>
2023-07-27 13:36:43 -0700

    added volunteers table tab

--- volunteerverse-ui/src/pages/Org/OrgProjectDetails/VolunteersTable.tsx
+++ volunteerverse-ui/src/pages/Org/OrgProjectDetails/VolunteersTable.tsx
@@ -1,29 +1,56 @@
-import { Avatar, Badge, Table, Group, Text, Select, ScrollArea, Paper, useMantineTheme, Button, Flex, Container, CloseButton, ActionIcon } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
+import { Avatar, Badge, Table, Group, Text, Select, ScrollArea, Paper, useMantineTheme, Button, Flex, Container, CloseButton, ActionIcon, Modal, Title } from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import { VolunteerProp } from '../../../props/users';
-import { IconCheck } from '@tabler/icons-react';
+import { IconCheck, IconX } from '@tabler/icons-react';
+import { useState } from 'react';
+import { ApiResponseProp, apiClient } from '../../../services/ApiClient';
+import { useParams } from 'react-router-dom';
 
 interface VolunteerTableProp {
     data: { avatar: string; firstName: string; email: string; role: string }[];
 }
-
-
 const rolesData = ['Manager', 'Collaborator', 'Contractor'];
-const approvedVolunteer = () => {
-
-};
-const rejectVolunteer = () => {
-
-};
 
 export function VolunteersTable({ volunteerData }: { volunteerData: VolunteerProp[] }) {
+    const {projectId} = useParams();
     const theme = useMantineTheme();
     const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
     const openVolunteerProfileModal = (action : string) => {
         console.log("open volunteer proile modal here and get result")
     }
-
-    const rows = volunteerData.map((volunteer) => (
+    
+    const VolunteerTableRows = volunteerData.map((volunteer) => {
+        const [showApprovedLoader, {open : openApprovedButtonLoader, close: closeApprovedButtonLoader}] = useDisclosure(false);
+        const [showRejectedLoader, {open : openRejectedButtonLoader, close: closeRejectedButtonLoader}] = useDisclosure(false);
+        const [isApproved, setIsApproved] = useState<boolean | undefined>(volunteer.approved)
+        const toggleVolunteerApproval = (volunteerEmail : string) => {
+            apiClient.toggleVolunteerApproval(volunteerEmail, projectId || "").then(({success, data, statusCode, error} : ApiResponseProp) => {
+                if (success){
+                    /**
+                     * set the new card state
+                    */
+                   setIsApproved(data.approve);
+                } else{
+                    console.log("unable to toggle volunteer approval: ", error);
+                    closeApprovedButtonLoader();
+                    closeRejectedButtonLoader();
+                }
+            }).catch((error) => {
+                
+                console.log("an unexpected error occured while trying to approve volunteer: ", error)
+                closeApprovedButtonLoader();
+            });
+        }
+        const fetchCorrectStatusOption = (pendingOption:any, rejectedOption:any, approvedOption:any) => {
+            if (isApproved === undefined){
+                return pendingOption;
+            }else if (isApproved === false){
+                return rejectedOption;
+            }else if (isApproved === true){
+                return approvedOption;
+            }
+        }
+        return (
         <tr key={volunteer.firstName}>
             <td>
                 <Flex ml={"xl"} mr={"xl"} maw={20} gap={"md"} direction={"row"} >
@@ -43,17 +70,24 @@ export function VolunteersTable({ volunteerData }: { volunteerData: VolunteerPro
             </td>
             <td>
                 <Group position='center'>
-                    <Badge ml={"xl"} size={isMobile ? "md" : 'lg'} radius={"xl"} variant='light'>{ "Pending Approval"}</Badge>
+                    <Badge ml={"xl"} size={isMobile ? "md" : 'lg'} radius={"xl"} variant='light'>{ fetchCorrectStatusOption("Pending Approval", "Rejected", "Approved") }</Badge>
                     <Group spacing={10}>
-                        <ActionIcon onClick={approvedVolunteer} color='green' variant='outline' size={isMobile ? "md" : 'xl'} radius={"xl"}>
+                        <ActionIcon disabled={showRejectedLoader}
+                            loading={showApprovedLoader} 
+                            onClick={() => {openApprovedButtonLoader(); toggleVolunteerApproval(volunteer.email)}}
+                            color='green' variant='outline' size={isMobile ? "md" : 'xl'}
+                            radius={"xl"}>
                             <IconCheck />
                         </ActionIcon>
-                        <CloseButton onClick={rejectVolunteer} color='red' size={isMobile ? "md" : 'xl'} radius={"xl"} variant='outline' aria-label="Close modal" />
+                        <CloseButton disabled={showApprovedLoader} loading={showRejectedLoader}
+                            onClick={() => {openRejectedButtonLoader(); toggleVolunteerApproval(volunteer.email)}} color='red'
+                            size={isMobile ? "md" : 'xl'} radius={"xl"}
+                            variant='outline' aria-label="Close modal" />
                     </Group>
                 </Group>
             </td>
         </tr>
-    ));
+    )});
 
     return (
         <Paper radius={"lg"} ml={"auto"} mr={"auto"} maw={1000}>
@@ -65,10 +99,9 @@ export function VolunteersTable({ volunteerData }: { volunteerData: VolunteerPro
                             <th><Text fz={isMobile ? "md" : "xl"} align='center'>Status</Text></th>
                         </tr>
                     </thead>
-                    <tbody>{rows}</tbody>
+                    <tbody>{VolunteerTableRows}</tbody>
                 </Table>
             </ScrollArea>
-
         </Paper>
     );
-}
+};
