9f790f417195318bb47374fce76438b370e219ed
Kelechi <kelechi@utexas.edu>
2023-07-27 13:37:06 -0700

    added project details page for org (WIP)

--- volunteerverse-ui/src/pages/Org/OrgProjectDetails/OrgProjectDetails.tsx
+++ volunteerverse-ui/src/pages/Org/OrgProjectDetails/OrgProjectDetails.tsx
@@ -1,94 +1,169 @@
-import { createStyles, useMantineTheme } from "@mantine/styles";
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import {
+  Badge, Container, Divider,
+  Group, Image, Title, Text,
+  useMantineTheme,
+  createStyles,
+  LoadingOverlay,
+  Skeleton,
+  Button,
+  Flex,
+  Box
+} from "@mantine/core";
+import { VolunteerProjectProp } from "../../../props/projects";
+import { ApiResponseProp, apiClient } from "../../../services/ApiClient";
+import GoBackButton from "../../../components/GoBackButton";
+import NotAuthorized from "../../NotAuthorized";
+import { projectDetailsData } from "../../Volunteer/Home/data";
 import { useAuthenticationUserProp } from "../../../services/hooks/useAuthentication";
 
-import { Tabs, Title } from '@mantine/core';
-import { IconPhoto, IconMessageCircle, IconSettings, IconUsersGroup, IconUsers, IconFaceId, IconUser, IconDashboard, IconFile, IconSubtask, IconActivity, IconHexagon3d, IconAlertCircle, IconCartCheck } from '@tabler/icons-react';
-import { useMediaQuery } from "@mantine/hooks";
-import { VolunteerProp } from "../../../props/users";
-import { useEffect, useState } from "react";
-import GoBackButton from "../../../components/GoBackButton";
-import VolunteerProjectDetails from "../../Volunteer/VolunteerProjectDetails";
-import { VolunteersTable } from "./VolunteersTable";
-const userList: VolunteerProp[] = [
-  {
-    email: 'user1@example.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    imageUrl: 'https://example.com/user1.jpg',
-    bio: 'A software engineer with a passion for coding.',
-    approved : undefined,
-  },
-  {
-    email: 'user2@example.com',
-    firstName: 'Alice',
-    lastName: 'Smith',
-    imageUrl: 'https://example.com/user2.jpg',
-    bio: 'Designer and artist exploring creativity.',
-    approved : undefined,
-  },
-  {
-    email: 'user3@example.com',
-    firstName: 'Mike',
-    lastName: 'Johnson',
-    imageUrl: 'https://example.com/user3.jpg',
-    bio: 'Aspiring entrepreneur and business enthusiast.',
-    approved : undefined,
-  },
-  {
-    email: 'user4@example.com',
-    firstName: 'Sarah',
-    lastName: 'Brown',
-    imageUrl: 'https://example.com/user4.jpg',
-    bio: 'Nature lover and outdoor enthusiast.',
-    approved : undefined,
-  },
-  {
-    email: 'user5@example.com',
-    firstName: 'Alex',
-    lastName: 'Lee',
-    imageUrl: 'https://example.com/user5.jpg',
-    bio: 'Passionate about technology and innovation.',
-    approved : undefined,
-  },
-];
+
+
 const useStyles = createStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: `calc(${theme.spacing.xl} * 1.5)`,
+    alignItems: "center",
+    // height: "100%"
+  },
+  textContent: {
 
-}));
+  },
+  tag: {
+    zIndex: 2,
+    transform: "scale(0.99)",
+    transition: "all 200ms ease-in-out",
+    "&:hover": {
+      transform: "scale(1)",
+      boxShadow: theme.shadows.md,
+      cursor: "pointer"
+    }
+  }
 
-function OrgProjectDetails({ isAuth, user}: {isAuth : boolean, user : useAuthenticationUserProp}) {
-    const theme = useMantineTheme();
-    const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-    const [projectVolunteers, setProjectVolunteers] = useState<undefined | VolunteerProp[]>(undefined);
+}))
 
-    useEffect(() => {
-        // get interested volunteers here;
-        // decide whether to approve and reject
-    }, [])
+function VolunteerProjectDetails({ isAuth, user }: { isAuth: boolean, user: useAuthenticationUserProp }) {
+  const params = useParams();
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const [buttonIsLoading, { open: showLoadingButton, close: hideLoadingButton }] = useDisclosure(false);
+  const { projectId } = params;
+  const { classes } = useStyles();
 
-    // two tabs, with left tab showing volunteers
-    const { classes } = useStyles();
+  const [project, setProject] = useState<VolunteerProjectProp | undefined>(projectId ? {
+    title: "",
+    createdAt: undefined,
+    description: "",
+    orgName: "",
+    orgUrl: "",
+    imageUrl: "",
+    orgDesc: "",
+    founders: "",
+    tags: [""],
+    interested: false,
+    approved: false,
+    id: -1
+  } : undefined);
+  const toggleProjectInterest = () => {
+    // updates intersted_volunteers database table with 
+    // the volunteer's interest
+    showLoadingButton();
+    if (projectId) {
+      apiClient.updateProjectInterestByUser(projectId).then((response : ApiResponseProp) => {
+        const { data, success, statusCode, error } = response;
+        console.log("updating interested from ", project?.interested, " to ", !project?.interested)
+        if (success) {
+          console.log("toggling user interest");
 
-    return (
-      
-        <>
-        <GoBackButton />
-        <Tabs p={"lg"} variant="default" radius="md" defaultValue="volunteers">
-          <Tabs.List position="center" >
-            <Tabs.Tab value="project" icon={<IconCartCheck size={isMobile ? "1rem" : "2rem"} />}><Title weight={500} order={isMobile ? 5 : 3}>Project Details</Title></Tabs.Tab>
-            <Tabs.Tab value="volunteers" icon={<IconUser size={isMobile ? "1rem" : "2rem"} />}><Title weight={500} order={isMobile ? 5 : 3}>{`${projectVolunteers?.length || ""} Volunteers`}</Title></Tabs.Tab>
-          </Tabs.List>
-    
-          <Tabs.Panel pt={"xs"} value="project">
-            {/* <VolunteerProjectDetails isAuth={false} user={undefined} /> */}
-          </Tabs.Panel>
-    
-          <Tabs.Panel pt={"xs"} value="volunteers">
-            <VolunteersTable   volunteerData={userList}/>
-          </Tabs.Panel>
-    
-        </Tabs>
-        </>
-      );
+          setProject((initialData) => {
+            if (initialData) {
+              return {
+                ...initialData,
+                interested: !initialData.interested
+              }
+            }
+            return undefined;
+          });
+          hideLoadingButton();
+        } else {
+          console.log("error status code: ", statusCode);
+          console.log('unable to update users interest.', error)
+          hideLoadingButton();
+        }
+      })
+    };
+  }
+  useEffect(() => {
+    // fecthes project by id and updates state variable 
+    // if successful
+
+    console.log("found project id: ", projectId)
+    const fetchProjectById = async () => {
+      if (projectId) {
+        const { data, success, statusCode, error } = await apiClient.fetchProjectById(projectId);
+        if (success) {
+          // update projects if successful
+          console.log("found project", data);
+          setProject(data);
+        } else if (statusCode === 400) {
+          // set project to undefined if unsuccessful
+          setProject(undefined)
+        } else {
+          // same as above, but just for error logging
+          console.log("an unexpected error occured: ", error)
+        }
+      }
+    }
+    /**
+     * @todo: connect to backend
+     */
+    setProject(projectDetailsData);
+    // fetchProjectById();
+  }, []);
+  console.log(project)
+  return (project === undefined || isAuth === false) ? <NotAuthorized /> : (
+    <Box p={0} m={0}>
+      <GoBackButton mb={"md"} w={"100%"} maw={200} />
+      <Skeleton visible={project.title === ""}>
+        <Container className={classes.container} px={isMobile ? 0 : "md"}>
+          <Flex gap={isMobile ? "sm" : "md"} direction={"column"} w={"100%"} align={"center"}>
+            <Image fit="cover" radius={"xl"} withPlaceholder src={project?.imageUrl} width={isMobile ? "100%" : "100%"} height={isMobile ? 300 : 500} />
+            <Group variant="filled" >
+              {project?.tags.map((tag) => {
+                return (
+                  <Badge className={classes.tag} key={tag} variant="light" size={isMobile ? "lg" : "xl"} >{tag}</Badge>
+                )
+              })}
+            </Group>
+          </Flex>
+          <Button
+            w={"100%"} maw={400} radius={"lg"}
+            size="xl" compact sx={{
+              backgroundColor: project.interested ? `${theme.colors.green[6]}` : `${theme.colors.violet[7]}`,
+              transition: "all 100ms ease-in-out"
+            }}
+            loading={buttonIsLoading}
+            onClick={toggleProjectInterest}>
+            {project.interested ? "Remove Interest" : "Express Interest"}</Button>
+          <Flex align={"start"} direction={"column"} className={classes.textContent} w={"100%"}>
+            <Text align="center">Posted: {project?.createdAt}</Text>
+            <Title order={1}>{project?.title}</Title>
+            <Title p={isMobile ? "xs" : "sm"} order={4}>by {project?.orgName}</Title>
+            <Title order={2}>Description:</Title>
+            <Text p={isMobile ? "xs" : "sm"}>{project?.description}</Text>
+            <Divider />
+            <Title align="left" order={2}>About {project?.orgName}:</Title>
+            <Text align="left" p={isMobile ? "xs" : "sm"}>{project?.orgDesc}</Text>
+          </Flex>
+        </Container>
+      </Skeleton>
+      <LoadingOverlay visible={project.title === ""} radius={"xl"} overlayBlur={2} overlayOpacity={0.3} loaderProps={{ size: "xl" }} />
+    </Box>
+
+  )
 }
 
-export default OrgProjectDetails
+export default VolunteerProjectDetails
