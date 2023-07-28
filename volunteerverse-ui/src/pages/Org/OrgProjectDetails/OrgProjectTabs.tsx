f734a257e11d76c99913c11c327f536908da29bd
Kelechi <kelechi@utexas.edu>
2023-07-28 10:38:34 -0700

    added org projects tab

--- volunteerverse-ui/src/pages/Org/OrgProjectDetails/OrgProjectTabs.tsx
+++ volunteerverse-ui/src/pages/Org/OrgProjectDetails/OrgProjectTabs.tsx
@@ -5,11 +5,12 @@ import { Tabs, Title } from '@mantine/core';
 import { IconPhoto, IconMessageCircle, IconSettings, IconUsersGroup, IconUsers, IconFaceId, IconUser, IconDashboard, IconFile, IconSubtask, IconActivity, IconHexagon3d, IconAlertCircle, IconCartCheck } from '@tabler/icons-react';
 import { useMediaQuery } from "@mantine/hooks";
 import { VolunteerProp } from "../../../props/users";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import GoBackButton from "../../../components/GoBackButton";
 import VolunteerProjectDetails from "../../Volunteer/VolunteerProjectDetails";
 import { VolunteersTable } from "./VolunteersTable";
 import OrgProjectDetails from "./OrgProjectDetails";
+import { AuthenticationContext } from "../../../context/AuthenicationContext";
 const userList: VolunteerProp[] = [
   {
     email: 'user1@example.com',
@@ -17,7 +18,7 @@ const userList: VolunteerProp[] = [
     lastName: 'Doe',
     imageUrl: 'https://example.com/user1.jpg',
     bio: 'A software engineer with a passion for coding.',
-    approved : undefined,
+    approved: undefined,
   },
   {
     email: 'user2@example.com',
@@ -25,7 +26,7 @@ const userList: VolunteerProp[] = [
     lastName: 'Smith',
     imageUrl: 'https://example.com/user2.jpg',
     bio: 'Designer and artist exploring creativity.',
-    approved : undefined,
+    approved: undefined,
   },
   {
     email: 'user3@example.com',
@@ -33,7 +34,7 @@ const userList: VolunteerProp[] = [
     lastName: 'Johnson',
     imageUrl: 'https://example.com/user3.jpg',
     bio: 'Aspiring entrepreneur and business enthusiast.',
-    approved : undefined,
+    approved: undefined,
   },
   {
     email: 'user4@example.com',
@@ -41,7 +42,7 @@ const userList: VolunteerProp[] = [
     lastName: 'Brown',
     imageUrl: 'https://example.com/user4.jpg',
     bio: 'Nature lover and outdoor enthusiast.',
-    approved : undefined,
+    approved: undefined,
   },
   {
     email: 'user5@example.com',
@@ -49,47 +50,48 @@ const userList: VolunteerProp[] = [
     lastName: 'Lee',
     imageUrl: 'https://example.com/user5.jpg',
     bio: 'Passionate about technology and innovation.',
-    approved : undefined,
+    approved: undefined,
   },
 ];
 const useStyles = createStyles((theme) => ({
 
 }));
 
-function OrgProjectDetailsTabs({ isAuth, user }: { isAuth: boolean, user: useAuthenticationUserProp }) {
-    const theme = useMantineTheme();
-    const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
-    const [projectVolunteers, setProjectVolunteers] = useState<undefined | VolunteerProp[]>(undefined);
+function OrgProjectDetailsTabs() {
+  // const { isAuth, user } = useContext(AuthenticationContext);
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const [projectVolunteers, setProjectVolunteers] = useState<undefined | VolunteerProp[]>(undefined);
 
-    useEffect(() => {
-        // get interested volunteers here;
-        // decide whether to approve and reject
-    }, [])
+  useEffect(() => {
+    // get interested volunteers here;
+    // decide whether to approve and reject
+  }, [])
 
-    // two tabs, with left tab showing volunteers
-    const { classes } = useStyles();
+  // two tabs, with left tab showing volunteers
+  const { classes } = useStyles();
 
-    return (
-      
-        <>
-        <GoBackButton />
-        <Tabs p={"lg"} variant="default" radius="md" defaultValue="volunteers">
-          <Tabs.List position="center" >
-            <Tabs.Tab value="project" ><Title weight={500} order={isMobile ? 5 : 3}>Project Details</Title></Tabs.Tab>
-            <Tabs.Tab value="volunteers" icon={<IconUser size={isMobile ? "1rem" : "2rem"} />}><Title weight={500} order={isMobile ? 5 : 3}>{`${projectVolunteers?.length || ""} Volunteers`}</Title></Tabs.Tab>
-          </Tabs.List>
-    
-          <Tabs.Panel pt={"xs"} value="project">
-            <VolunteerProjectDetails isAuth={isAuth} user={user} />
-          </Tabs.Panel>
-    
-          <Tabs.Panel pt={"xs"} value="volunteers">
-            <VolunteersTable   volunteerData={userList}/>
-          </Tabs.Panel>
-    
-        </Tabs>
-        </>
-      );
+  return (
+
+    <>
+      <GoBackButton />
+      <Tabs p={"lg"} variant="default" radius="md" defaultValue="volunteers">
+        <Tabs.List position="center" >
+          <Tabs.Tab value="project" ><Title weight={500} order={isMobile ? 5 : 3}>Project Details</Title></Tabs.Tab>
+          <Tabs.Tab value="volunteers" icon={<IconUser size={isMobile ? "1rem" : "2rem"} />}><Title weight={500} order={isMobile ? 5 : 3}>{`${projectVolunteers?.length || ""} Volunteers`}</Title></Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel pt={"xs"} value="project">
+          <VolunteerProjectDetails />
+        </Tabs.Panel>
+
+        <Tabs.Panel pt={"xs"} value="volunteers">
+          <VolunteersTable volunteerData={userList} />
+        </Tabs.Panel>
+
+      </Tabs>
+    </>
+  );
 }
 
 export default OrgProjectDetailsTabs
