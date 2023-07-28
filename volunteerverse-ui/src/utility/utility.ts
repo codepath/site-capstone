0cf7e2bc8b2ea33db25da501c08b380d6e4c5b92
Kelechi <kelechi@utexas.edu>
2023-07-28 10:42:17 -0700

    usecontext refactoring

--- volunteerverse-ui/src/utility/utility.ts
+++ volunteerverse-ui/src/utility/utility.ts
@@ -4,15 +4,15 @@ export const fetchCorrectUserOption =  (
     unAuthOption: any, 
     volunteerOption: any, 
     organizationOption: any, 
-    props : {isAuth : boolean, user : useAuthenticationUserProp}) => {
-        console.log("choosing option")
-        if (props.isAuth === false){
+    props : {isAuth : boolean | undefined, user : useAuthenticationUserProp | undefined}) => {
+        console.log("choosing option", props)
+        if (!props.isAuth || !props.user){
             return unAuthOption;
         }else if (props.user.userType === "volunteer"){
             return volunteerOption;
         }else if (props.user.userType === "organization"){
             return organizationOption;
         } else{
-            console.log("ERROR: unable to determine correct user option... recieved: \n\nuser:", props.user );
+            console.log("ERROR: unable to determine correct userType + isAuth option... recieved: \n\nuser:", props.user );
         }
 }
