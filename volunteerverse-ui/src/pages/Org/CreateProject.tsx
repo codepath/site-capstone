54536fed39f075e3e66dde73fe0ad4efed353006
Kelechi <kelechi@utexas.edu>
2023-07-28 10:32:56 -0700

    added create project page (WIP)

--- volunteerverse-ui/src/pages/Org/CreateProject.tsx
+++ volunteerverse-ui/src/pages/Org/CreateProject.tsx
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useAuthenticationUserProp } from '../../services/hooks/useAuthentication'
 import { useForm } from '@mantine/form';
 import { ProjectFormValues } from '../../props/forms';
 import {
   Button, Container, FileButton,
   Flex, MultiSelect, TextInput,
-  Textarea, Title, Image, createStyles, Paper, Divider, ColorSchemeProvider
+  Textarea, Title, Image, createStyles, Paper, Divider, ColorSchemeProvider, useMantineTheme
 } from '@mantine/core';
 import { apiClient } from '../../services/ApiClient';
-
+import GoBackButton from '../../components/GoBackButton';
+import { useMediaQuery } from '@mantine/hooks';
+import { useNavigate } from 'react-router-dom';
+import { AuthenticationContext } from '../../context/AuthenicationContext';
+/**
+ * @todo: 
+ * - make context for use authenication instead of passing down props?
+ * - use context to insert project details page into tab view for org owners
+ * - fully connect all of backend to front end starting with an org registertion, then student pages, then all of org pages
+ * - then combining the two pages together. Then handle loading states and styling.
+ * - and then a student pulling seeing those projects, 
+ * - work on sending 
+ */
 const useStyles = createStyles((theme) => ({
   container: {
-
   },
   image: {
     display: "flex",
@@ -28,7 +39,8 @@ const useStyles = createStyles((theme) => ({
   }
 }))
 
-function CreateProject({ isAuth, user }: { isAuth: boolean, user: useAuthenticationUserProp }) {
+function CreateProject() {
+  const {isAuth, user} = useContext(AuthenticationContext);
   const form = useForm<ProjectFormValues>({
     initialValues: {
       title: "",
@@ -48,99 +60,124 @@ function CreateProject({ isAuth, user }: { isAuth: boolean, user: useAuthenticat
   });
   const { classes } = useStyles();
   const [tags, setTags] = useState<string[]>([]);
-  const createNewProject = () => {
-    if (!form.validate().hasErrors){
+  const theme = useMantineTheme();
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const navigate = useNavigate();
 
+  const createNewProject = () => {
+    // makes reques to create a new project
+    // if the form is valid
+    // show loader here
+    if (!form.validate().hasErrors) {
       console.log("creating new project with the following form values: ", form.values)
-    } else{
+      const payload = {
+        ...form.values,
+        name: form.values.title,
+      }
+      apiClient.createProject(form.values).then(({ data, success, error, statusCode }) => {
+        if (success) {
+          console.log("created new project")
+          navigate("/");
+        } else {
+          console.log("failed to create new project. show error notification here")
+        }
+      }).catch((error) => {
+        console.log("a very unexpected error has occured", error)
+      })
+    } else {
       console.log("form has errros: ", form.errors)
     }
+    // close loader here
   }
+
   useEffect(() => {
-    apiClient.fetchAllTags().then(({data, success, error}) => {
+    apiClient.fetchAllTags().then(({ data, success, error }) => {
       // fecthes all all tags from db then sets state
-      if (success){
+      if (success) {
         setTags(data.tags);
-      }else{
+      } else {
         console.log("unable to retrieve all tags. error: ", error)
       }
     });
   }, [])
   return (
-    <Paper maw={900} mx={"auto"} radius={"lg"} p={"xl"}>
-    <Container mt={"xl"}>
-      <Title className={classes.title} mb={"xl"} >Create a New Project</Title>
-      <Divider py={"md"} />
-      <Flex direction={"column"}>
-        <Flex direction={"column"} gap={"md"} align={"center"}>
-          <Image
-            width={"100%"}
-            height={300}
-            withPlaceholder
-            radius={"lg"} 
-            mb={"md"}/>
-          <Flex
-            direction={"column"}
-            justify={"center"}
-            align={"center"}
-            gap={"sm"}
-            mb={"xl"}>
-            <FileButton {...form.getInputProps("imageFile")}>
-              {(props) => <Button variant="light"
+    <>
+      <GoBackButton mb={"md"}/>
+      <Paper maw={900} mx={"auto"} radius={"lg"} p={"xl"}>
+        <Container mt={"xl"}>
+          <Title className={classes.title} mb={"xl"} >Create a New Project</Title>
+          <Divider py={"md"} />
+          <Flex direction={"column"}>
+            <Flex direction={"column"} gap={"md"} align={"center"}>
+              <Image
+                width={"100%"}
+                height={300}
+                withPlaceholder
                 radius={"lg"}
-                styles={{
-                  root: {
-                    maxWidth: "30rem"
-                  }
-                }}
-                {...props}>
-                  {form.values.imageFile?.name || "Upload Project Cover"}</Button>}
-            </FileButton>
+                mb={"md"} />
+              <Flex
+                direction={"column"}
+                justify={"center"}
+                align={"center"}
+                gap={"sm"}
+                mb={"xl"}>
+                <FileButton {...form.getInputProps("imageFile")}>
+                  {(props) => <Button variant="light"
+                    radius={"lg"}
+                    styles={{
+                      root: {
+                        maxWidth: "30rem"
+                      }
+                    }}
+                    {...props}>
+                    {form.values.imageFile?.name || "Upload Project Cover"}</Button>}
+                </FileButton>
+              </Flex>
+            </Flex>
+            <TextInput
+              size={isMobile ? 'sm' : 'md'}
+              radius={"lg"}
+              withAsterisk
+              label="Project Title"
+              placeholder="Your project title"
+              description="Full stack developer needed for..."
+              {...form.getInputProps('title')}
+              mb={"md"} />
+            <TextInput
+              type='number'
+              min={1}
+              size={isMobile ? 'sm' : 'md'}
+              radius={"lg"}
+              label="Volunteer Capacity (optional)"
+              placeholder="Maximum volunteers needed"
+              description=""
+              {...form.getInputProps('requestedPeople')}
+              mb={"md"} />
+            <MultiSelect
+              {...form.getInputProps("tags")}
+              size={isMobile ? 'sm' : 'md'}
+              radius={"lg"}
+              searchable
+              data={tags}
+              label="Project Tags"
+              placeholder="Select tags relevant to this project"
+              mb={"md"}
+            />
+            <Textarea
+              radius={"lg"}
+              size={isMobile ? 'sm' : 'md'}
+              withAsterisk
+              label="Project Description:"
+              placeholder={`Hey there, we're a ${user?.userType} looking to...`}
+              description="Max: 500 words"
+              minRows={5}
+              {...form.getInputProps('desc')}
+            />
           </Flex>
-        </Flex>
-        <TextInput
-                size='md'
-                radius={"lg"}
-                withAsterisk
-                label="Project Title"
-                placeholder="Your project title"
-                description="Full stack developer needed for..."
-            {...form.getInputProps('title')}
-            mb={"md"} />
-          <TextInput
-            type='number'
-            min={1}
-            size='md'
-            radius={"lg"}
-            label="Volunteer Capacity (optional)"
-            placeholder="Maximum volunteers needed"
-            description=""
-            {...form.getInputProps('requestedPeople')}
-            mb={"md"} />
-          <MultiSelect
-            {...form.getInputProps("tags")}
-            size='md'
-            radius={"lg"}
-            searchable
-            data={tags}
-            label="Project Tags"
-            placeholder="Select tags relevant to this project"
-            mb={"md"}
-             />
-        <Textarea
-          radius={"lg"}
-          size='md'
-          withAsterisk
-          label="Project Description:"
-          placeholder= {`Hey there, we're a ${user.userType} looking to...`}
-          description="Max: 500 words"
-          minRows={5}
-          {...form.getInputProps('desc')}
-        />
-      </Flex>
-    </Container>
-    <Button onClick={createNewProject} radius={"lg"} size='lg' mt={"xl"}>Create Project</Button>
-    </Paper>
+        </Container>
+        <Button onClick={createNewProject} radius={"lg"} size='lg' mt={"xl"}>Create Project</Button>
+      </Paper>
+    </>
   )
 
 }
