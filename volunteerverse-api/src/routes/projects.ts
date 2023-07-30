b1f2aa56c259430c5347a1b7d43294adf10ce1e3
Kelechi <kelechi@utexas.edu>
2023-07-30 16:35:06 -0700

    changes from merge

--- volunteerverse-api/src/routes/projects.ts
+++ volunteerverse-api/src/routes/projects.ts
@@ -23,26 +23,15 @@ projectRoutes.post("/register", async function (req, res, next){
 
 
 
-/**route that returns project information given the project id */
-projectRoutes.get(
-  "/:projectId",
-  requireAuthenticatedUser,
-  async function (req, res, next) {
-    const projectId = parseInt(req.params.projectId);
-    const { user_type, email } = res.locals.user;
-    console.log("USERTYPE", user_type);
-    try {
-      const project = await Projects.fetchProjectByProjectId(
-        projectId,
-        user_type,
-        email
-      );
-      res.status(201).json(project);
-    } catch (error) {
-      next(error);
-    }
+/** route that gets all the project tags in database */
+projectRoutes.get("/tags", async function (req, res, next) {
+  try {
+    const tags = await Projects.getAllProjectTags();
+    res.status(201).json({ tags: tags });
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 /**route that gets all projects with given tag */
 projectRoutes.get(
@@ -58,29 +47,41 @@ projectRoutes.get(
       res.status(404).json({ error: "Projects with given tag not found" });
     }
   }
-);
-
-/**route that handles searching by a term */
-projectRoutes.get(
-  "/search/:searchTerm",
-  requireAuthenticatedUser,
-  async function (req, res, next) {
-    const search = req.params.searchTerm;
-    try {
-      const projects = await Projects.searchProjects(search);
-      res.status(201).json(projects);
-    } catch (error) {
-      next(error);
+  );
+  
+  /**route that handles searching by a term */
+  projectRoutes.get(
+    "/search/:searchTerm",
+    requireAuthenticatedUser,
+    async function (req, res, next) {
+      const search = req.params.searchTerm;
+      try {
+        const projects = await Projects.searchProjects(search);
+        res.status(201).json(projects);
+      } catch (error) {
+        next(error);
+      }
     }
-  }
-);
-
-/** route that gets all the project tags in database */
-projectRoutes.get("/tags", async function (req, res, next) {
-  try {
-    const tags = await Projects.getAllProjectTags();
-    res.status(201).json({ tags: tags });
-  } catch (error) {
-    next(error);
-  }
-});
+    );
+    /**route that returns project information given the project id */
+    projectRoutes.get(
+      "/:projectId",
+      requireAuthenticatedUser,
+      async function (req, res, next) {
+        const projectId = parseInt(req.params.projectId);
+        const { user_type, email } = res.locals.user;
+        console.log("USERTYPE", user_type);
+        try {
+          const project = await Projects.fetchProjectByProjectId(
+            projectId,
+            user_type,
+            email
+          );
+          res.status(201).json(project);
+        } catch (error) {
+          next(error);
+        }
+      }
+    );
+    
+    
