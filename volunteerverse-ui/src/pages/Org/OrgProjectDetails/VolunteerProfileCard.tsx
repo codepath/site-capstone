dc468214ee35287fbc305f05725b62da6d32b2c0
Kelechi <kelechi@utexas.edu>
2023-07-31 12:50:35 -0700

    added volunteer profile card

---
+++ volunteerverse-ui/src/pages/Org/OrgProjectDetails/VolunteerProfileCard.tsx
@@ -0,0 +1,25 @@
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Group, Button, Paper, Avatar, Text } from '@mantine/core';
+import { VolunteerProp } from '../../../props/users';
+
+export default function VolunteerProfileCard({volunteerProfile, closeModal} : {volunteerProfile: VolunteerProp, closeModal: () => void}) {
+const { firstName, lastName, imageUrl, bio } = volunteerProfile;
+    return (
+        <Paper
+            radius="xl"
+            withBorder
+            p="lg"
+            sx={(theme) => ({
+                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+            })}>
+            <Avatar src={imageUrl} size={120} radius={120} mx="auto" />
+            <Text ta="center" fz="lg" weight={500} mt="md">
+                {`${firstName} ${lastName}`}
+            </Text>
+            <Text ta="center" c="dimmed" fz="sm">
+                {bio}
+            </Text>
+            <Button onClick={closeModal} variant='light' mt={"lg"} radius={"xl"}>Close</Button>
+        </Paper>
+    );
+}
