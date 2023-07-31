9e9619c2d357637f76c1aefc4c5102a48ee51feb
Kelechi <kelechi@utexas.edu>
2023-07-30 19:04:11 -0700

    added fetchPrettyTime utility function

--- volunteerverse-ui/src/utility/utility.ts
+++ volunteerverse-ui/src/utility/utility.ts
@@ -1,4 +1,5 @@
 import { useAuthenticationUserProp } from "../services/hooks/useAuthentication";
+import moment from "moment";
 
 export const fetchCorrectUserOption =  (
     unAuthOption: any, 
@@ -15,4 +16,12 @@ export const fetchCorrectUserOption =  (
         } else{
             console.log("ERROR: unable to determine correct userType + isAuth option... recieved: \n\nuser:", props.user );
         }
+}
+export const fetchPrettyTime = (timestamp :  number) => {
+    /**
+     * @description: converts epcoch timestamp into a nice 
+     * human readable time format using moment.js
+     */
+    console.log(timestamp)
+    return moment(timestamp).fromNow();
 }
