99049978f5bab5ad76eadeb9b8f223cd9b37236a
Kelechi <kelechi@utexas.edu>
2023-08-02 16:43:23 -0700

    commneted out fetch correct user optoin

--- volunteerverse-ui/src/utility/utility.ts
+++ volunteerverse-ui/src/utility/utility.ts
@@ -2,22 +2,23 @@ import { notifications } from "@mantine/notifications";
 import { useAuthenticationUserProp } from "../services/hooks/useAuthentication";
 import moment from "moment";
 
-export const fetchCorrectUserOption =  (
-    unAuthOption: any, 
-    volunteerOption: any, 
-    organizationOption: any, 
-    props : {isAuth : boolean | undefined, user : useAuthenticationUserProp | undefined}) => {
-        console.log("choosing option", props)
-        if (!props.isAuth || !props.user){
-            return unAuthOption;
-        }else if (props.user.userType === "volunteer"){
-            return volunteerOption;
-        }else if (props.user.userType === "organization"){
-            return organizationOption;
-        } else{
-            console.log("ERROR: unable to determine correct userType + isAuth option... recieved: \n\nuser:", props.user );
-        }
-}
+// export const fetchCorrectUserOption =  (
+    
+//     unAuthOption: any, 
+//     volunteerOption: any, 
+//     organizationOption: any, 
+//     props : {isAuth : boolean | undefined, user : useAuthenticationUserProp | undefined}) => {
+//         console.log("choosing option", props)
+//         if (!props.isAuth || !props.user){
+//             return unAuthOption;
+//         }else if (props.user.userType === "volunteer"){
+//             return volunteerOption;
+//         }else if (props.user.userType === "organization"){
+//             return organizationOption;
+//         } else{
+//             console.log("ERROR: unable to determine correct userType + isAuth option... recieved: \n\nuser:", props.user );
+//         }
+// }
 export const fetchPrettyTime = (timestamp :  number) => {
     /**
      * @description: converts epcoch timestamp into a nice 
