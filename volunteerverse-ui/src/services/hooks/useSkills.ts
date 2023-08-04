971393723b643df8df1fc46d73b0c87106019ac5
Kelechi <kelechi@utexas.edu>
2023-08-04 10:51:46 -0700

    added useSkills hook to fetch all project tags

---
+++ volunteerverse-ui/src/services/hooks/useSkills.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { apiClient } from "../ApiClient";
+
+export const useSkills = () : {value: string, label:  string}[] => {
+    // hook retrieves all skills from database and returns it each call
+    // (for multi-select inputs only)
+    const [skillsTags, setSkillsTags] = useState<string[]>([]);
+    useEffect(() => {
+        const getProjectTags = async () => {
+          apiClient.fetchAllTags().then(({ data, success, error, statusCode }) => {
+            // fecthes all all tags from db then sets state
+            if (success) {
+              console.log("setting tags: ", data.tags)
+              setSkillsTags(data.tags);
+            } else {
+                setSkillsTags([])
+              console.log("unable to retrieve all tags. error: ", {error, statusCode})
+            }
+          });
+        }
+        getProjectTags();
+      }, [])
+      // returns skills in a map that is
+    return skillsTags?.map((tag) => ({
+        value: tag.toLocaleLowerCase(),
+        label: tag
+    }));
+}
+
