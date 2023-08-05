3331bcd438971e8e5165833f67ad2ec1eb096fb4
Kelechi <kelechi@utexas.edu>
2023-08-04 22:32:31 -0700

    added production backend url link

--- volunteerverse-ui/constants.ts
+++ volunteerverse-ui/constants.ts
@@ -1,4 +1,4 @@
-const PRODUCTION_API_BASE_URL = undefined;
+const PRODUCTION_API_BASE_URL = "https://volunteerverse-backend.onrender.com";
 const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
 
 export const API_BASE_URL = PRODUCTION_API_BASE_URL || DEVELOPMENT_API_BASE_URL;
